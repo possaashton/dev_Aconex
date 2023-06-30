f879f0e2d555f8b14982d048497beae05766bac7
Ashton Possa <possaashton@gmail.com>
2023-06-30 01:55:42 +0100

    modified file main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+import axios from './lib/axios/axios';
+async function connectToAPI(apiId: string): Promise<void> {
+    const url = `https://ea1.aconex.com/html/includes/js/api.js?id=${apiId}`;
+  
+    try {
+      const response = await axios.get(url);
+      console.log(response.data);
+      // Handle the response data as needed
+    } catch (error) {
+      console.error('An error occurred while connecting to the API:', error);
+      // Handle the error appropriately
+    }
+  }
+  const apiId = '1687445908773';
+  connectToAPI(apiId);
+    
